2a9cee9bbce42c024e7d10b1bf7857f39f6d8085
xuebiao <xuebiao@beisen.com>
2021-11-07 22:52:09 +0800

    some model

---
+++ api/dbops/internal.go
@@ -0,0 +1,24 @@
+package dbops
+
+import (
+	"strconv"
+)
+
+func InsertSession(sid string, ttl int64, uname string) error {
+	ttlstr := strconv.FormatInt(ttl, 10)
+	stmtIns, err := dbConn.Prepare("INSERT INTO sessions (session_id, TTL, login_name) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtIns.Exec(sid, ttlstr, uname)
+	if err != nil {
+		return err
+	}
+
+	defer stmtIns.Close()
+	return nil
+}
+
+
+
---
+++ api/response.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func sendErrorResponse(w http.ResponseWriter){
+
+}
+
+func sendNormalResponse(w http.ResponseWriter){
+
+}
+
