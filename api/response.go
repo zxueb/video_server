49d6aeae6b0a0373676c219cb0530cbede934419
xuebiao <xuebiao@beisen.com>
2021-11-08 00:14:08 +0800

    提交

--- api/response.go
+++ api/response.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"io"
+	"encoding/json"
 	"net/http"
+	"github.com/zxueb/video_server/api/defs"
 )
 
-func sendErrorResponse(w http.ResponseWriter){
+func sendErrorResponse(w http.ResponseWriter, errResp defs.ErrResponse) {
+	w.WriteHeader(errResp.HttpSC)
 
+	resStr, _ := json.Marshal(&errResp.Error)
+	io.WriteString(w, string(resStr))
 }
 
-func sendNormalResponse(w http.ResponseWriter){
-
+func sendNormalResponse(w http.ResponseWriter, resp string, sc int) {
+	w.WriteHeader(sc)
+	io.WriteString(w, resp)
 }
 
