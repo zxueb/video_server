d8c7f9f42dbf8b4dcf70a7bd7b7734a721f7f206
xuebiao <xuebiao@beisen.com>
2021-11-08 00:18:56 +0800

    提交

--- api/handlers.go
+++ api/handlers.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"io"
+	"encoding/json"
 	"net/http"
+	"io/ioutil"
 	"github.com/julienschmidt/httprouter"
+	"github.com/zxueb/video_server/api/defs"
+	"github.com/zxueb/video_server/api/dbops"
+	"github.com/zxueb/video_server/api/session"
 )
 
-func CreateUser(w http.ResponseWriter,r *http.Request,p httprouter.Params){
-	io.WriteString(w,"Create user success!")
+func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	res, _ := ioutil.ReadAll(r.Body)
+	ubody := &defs.UserCredential{}
+
+	if err := json.Unmarshal(res, ubody); err != nil {
+		sendErrorResponse(w, defs.ErrorRequestBodyParseFailed)
+		return 
+	}
+
+	if err := dbops.AddUserCredential(ubody.Username, ubody.Pwd); err != nil {
+		sendErrorResponse(w, defs.ErrorDBError)
+		return
+	}
+
+	id := session.GenerateNewSessionId(ubody.Username)
+	su := &defs.SignedUp{Success: true, SessionId: id}
+
+	if resp, err := json.Marshal(su); err != nil {
+		sendErrorResponse(w, defs.ErrorInternalFaults)
+		return
+	} else {
+		sendNormalResponse(w, string(resp), 201)
+	}
 }
 
-func Login(w http.ResponseWriter,r *http.Request,p httprouter.Params){
+func Login(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	uname := p.ByName("user_name")
-	io.WriteString(w,uname)
+	io.WriteString(w, uname)
 }
