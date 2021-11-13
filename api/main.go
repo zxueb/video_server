6970712269d6c3f48721e27bdc5ca67b114a8ba8
xuebiao <xuebiao@beisen.com>
2021-11-14 00:31:03 +0800

    后续实现

--- api/main.go
+++ api/main.go
@@ -3,6 +3,7 @@ package main
 import(
 	"net/http"
 	"github.com/julienschmidt/httprouter"
+	"github.com/zxueb/video_server/api/session"
 )
 
 type middleWareHandler struct {
@@ -15,9 +16,8 @@ func NewMiddleWareHandler(r *httprouter.Router) http.Handler {
 	return m
 }
 
-func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//check session
-	validateUserSession(r)
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r * http.Request) {
+	ValidateUserSession(r)
 
 	m.r.ServeHTTP(w, r)
 }
@@ -25,15 +25,32 @@ func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
 
-	router.POST("/user",CreateUser)
+	router.POST("/user", CreateUser)
 
-	router.POST("/user/:user_name",Login)
-	
+	router.POST("/user/:username", Login)
+
+	router.GET("/user/:username", GetUserInfo)
+
+	router.POST("/user/:username/videos", AddNewVideo)
+
+	router.GET("/user/:username/videos", ListAllVideos)
+
+	router.DELETE("/user/:username/videos/:vid-id", DeleteVideo)
+
+	router.POST("/videos/:vid-id/comments", PostComment)
+
+	router.GET("/videos/:vid-id/comments", ShowComments)
+
 	return router
 }
 
-func main(){
+func  Prepare()  {
+	session.LoadSessionsFromDB()
+}
+
+func main() {
+	Prepare()
 	r := RegisterHandlers()
 	mh := NewMiddleWareHandler(r)
-	http.ListenAndServe(":8000",mh)
+	http.ListenAndServe(":8000", mh)
 }
---
+++ streamserver/defs.go
@@ -0,0 +1,7 @@
+package main 
+
+const (
+	//VIDEO_DIR = "./videos/"
+	VIDEO_DIR = "F:/BaiduNetdiskDownload/"
+	MAX_UPLOAD_SIZE = 1024 * 1024 * 200 //200MB
+)
